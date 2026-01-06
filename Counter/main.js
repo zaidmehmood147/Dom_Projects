e1ea875b1f768e4d41ce87edd89fc424f4b8cbf1
unknown <kzaid2918@gmail.com>
2026-01-06 14:04:17 +0500

    Counter added js

--- Counter/main.js
+++ Counter/main.js
@@ -1,2 +1,18 @@
 var counter = 0;
-var
+var countingnum = document.getElementById("countingnum");
+
+function plus(){
+    counter = counter + 1;
+    countingnum.innerText = counter; 
+}
+
+function minus() {
+    counter = counter - 1;
+    countingnum.innerText = counter  ;
+
+}
+
+function refresh( ) {
+    counter = counter * 0;
+    countingnum.innerText = counter ;
+}
